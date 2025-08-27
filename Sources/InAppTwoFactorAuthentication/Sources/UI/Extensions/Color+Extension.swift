c10aa86703003f96e186d47235978e02b7dcd3e1
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-08-27 11:33:15 +0200

    feat: Stabilized UI

---
+++ Sources/InAppTwoFactorAuthentication/Sources/UI/Extensions/Color+Extension.swift
@@ -0,0 +1,46 @@
+/*
+ iOS Features
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import SwiftUI
+
+extension Color {
+    enum Custom {
+        /// light: greyOrca / dark: greyRabbit
+        static let textPrimary = Color(
+            light: UIColor.greyOrca,
+            dark: UIColor.greyRabbit
+        )
+        /// light: greyElephant / dark: greyShark
+        static let textSecondary = Color(
+            light: UIColor.greyElephant,
+            dark: UIColor.greyShark
+        )
+
+        /// light: greyMouse / dark: greyOrca
+        static let divider = Color(
+            light: UIColor.greyMouse,
+            dark: UIColor.greyOrca
+        )
+
+        /// light: greyMouse / dark: greyOrca
+        static let cardOutline = Color(
+            light: UIColor.clear,
+            dark: UIColor.greyOrca
+        )
+    }
+}
